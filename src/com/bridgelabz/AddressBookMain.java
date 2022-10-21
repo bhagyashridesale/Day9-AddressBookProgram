88beeb420e4bd9708723eb37952d1458d942568f
bhagyashridesale <112363604+bhagyashridesale@users.noreply.github.com>
2022-10-21 22:26:32 +0530

    Added:CreateContactList

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,10 +1,9 @@
 package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
-
-    System.out.println("Welcome to addressBook java Program");
-    Contact contact = new Contact();
-		System.out.println( contact );
+        System.out.println("Welcome to addressBook java Program");
+        Contact contact = new Contact();
+        System.out.println( contact );
     }
 }
 
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -4,15 +4,18 @@ public class Contact {
     private String lastName;
     private String address;
     private String city;
-    private String zip;
+    private int zip;
     private String state;
     private String phoneNo;
     private String email;
 
+
     public Contact() {
         // TODO Auto-generated constructor stub
     }
-    public Contact(String firstName, String lastName, String address, String city, String zip, String state, String phoneNo,String email) {
+
+    public Contact(String firstName, String lastName, String address, String city, int zip, String state, String phoneNo,
+                   String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -22,8 +25,13 @@ public class Contact {
         this.phoneNo = phoneNo;
         this.email = email;
     }
-    @Override public String toString() {
+
+    @Override
+    public String toString() {
         return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
                 + ", zip=" + zip + ", state=" + state + ", phoneNo=" + phoneNo + ", email=" + email + "]";
     }
+
 }
+
+
